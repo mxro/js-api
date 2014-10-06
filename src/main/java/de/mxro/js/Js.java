8d5a92fce866c010bfcab7d5811194b3f0278749
mxro <mxro@nowhere.com>
2014-10-06 13:55:06 +1100

    Improving way engines are selected Autocommit Mon Oct 06 13:55:06 EST 2014

--- src/main/java/de/mxro/js/Js.java
+++ src/main/java/de/mxro/js/Js.java
@@ -1,11 +1,20 @@
 package de.mxro.js;
 
+import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 
 import de.mxro.js.internal.EngineSelector;
 
 public class Js {
 
+    /**
+     * Evaluates the supplied string as JavaScript script and returns the result
+     * value.
+     * 
+     * @param javaScript
+     * @return
+     * @see {@link ScriptEngine#eval(String)}
+     */
     public static Object eval(final String javaScript) {
 
         try {
