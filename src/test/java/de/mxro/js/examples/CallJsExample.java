c00d542340ee99aafbba2b7bb51d5372113f3f52
mxro <mxro@nowhere.com>
2015-01-14 09:49:32 +1100

    Adding Example.

---
+++ src/test/java/de/mxro/js/examples/CallJsExample.java
@@ -0,0 +1,15 @@
+package de.mxro.js.examples;
+
+import de.mxro.js.Js;
+
+public class CallJsExample {
+
+    public static void main(final String[] args) {
+
+        final Object res = Js.eval("var x = 1 + 1;");
+
+        System.out.println(res);
+
+    }
+
+}
